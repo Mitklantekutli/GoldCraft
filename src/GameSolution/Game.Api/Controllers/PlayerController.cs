79356ec66ba079227d22a58f8fa30bf6e0a91bde
shalin.v <helix.ru>
2018-01-25 19:15:35 +0300

    api troubles

---
+++ src/GameSolution/Game.Api/Controllers/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Game.Model.GameEvents;
+
+namespace Game.Api.Controllers
+{
+    public class PlayerController : ApiController
+    {
+        public IHttpActionResult Get()
+        {
+            var ps = PlayerModule.Players;
+            return Ok(ps);
+        }
+
+        // GET api/<controller>/5
+        public IHttpActionResult Get(int id)
+        {
+            var p = PlayerModule.Players.SingleOrDefault(x => x.Id == id);
+            return Ok(p);
+        }
+
+
+    }
+
+    public static class PlayerModule
+    {
+        private static int Index { get; set; }
+        public static List<Player> Players { get; set; }
+        static PlayerModule()
+        {
+            Players = new List<Player>();
+            Index = 1;
+        }
+
+        public static long Add(string player)
+        {
+            var p = new Player() { Id = Index++, Name = player };
+            Players.Add(p);
+            return p.Id;
+        }
+    }
+}
---
+++ src/GameSolution/Game.Api/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Game.Api.WebApiApplication" Language="C#" %>
