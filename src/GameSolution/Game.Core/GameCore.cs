7320b8e59ea40b9a42cb78d7708064fc3e5327c3
shalin.v <helix.ru>
2018-01-25 18:04:32 +0300

    class model

---
+++ src/GameSolution/Game.Core/GameCore.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Game.Core
+{
+    public class GameCore
+    {
+        private bool Active { get; set; }
+
+        public Model.GameEvents.Game Game { get; set; }
+
+
+        public double Time { get; set; }
+        public double Delta { get; set; }
+
+        public GameCore()
+        {
+            var starter = new Task(() =>
+            {
+                while (Active)
+                {
+                    Tick();
+                    Thread.Sleep((int)Delta);
+                }
+            });
+            starter.Start();
+        }
+
+        public void Tick()
+        {
+            Time += Delta;
+        }
+
+        public void Start()
+        {
+            Active = true;
+        }
+
+        public void Stop()
+        {
+            Active = false;
+        }
+    }
+}
